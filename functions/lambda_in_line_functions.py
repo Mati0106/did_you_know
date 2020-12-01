2b8bda4a90f3ec16dbc8b6f1740c4beea698ba2e
mateuszserocki-kinesso <mateusz.serocki@kinesso.com>
2020-12-01 17:01:26 +0100

    step 2

--- functions/lambda_in_line_functions.py
+++ functions/lambda_in_line_functions.py
@@ -14,26 +14,58 @@ That script including pro tips for creation of function including
 
 
 # Use function with any number of arguments
-def avg(first,*rest):
-    return (first + sum(rest))/(1+len(rest))
+def avg(first, *rest):
+    return (first + sum(rest)) / (1 + len(rest))
 
-avg(1,2)
+
+avg(1, 2)
 # or
-avg(1,2,3,4,5)
+avg(1, 2, 3, 4, 5)
+
+
+# function that only accept keyword arguments
+def some_function(some_atribute, *, specified_atribiute):
+    'Receives a message'
+    pass
+
+
+try:
+    some_function(1024, True)
+except TypeError:
+    print(TypeError)
+some_function(1024, specified_atribiute=True)
+
+# custom sort function inside a lite
+names = ['Aly Sady', 'Gru Bubo', 'Abi Luko']
+sorted_names = sorted(names, key=lambda name: name.split()[-1].lower())
+
+# use function with some default attributes but change other ones
+from functools import partial
+
+
+def other_function(a, b, c, d):
+    print(a, b, c, d)
+
+
+func_with_def_atr = partial(other_function, 1)
+func_with_def_atr(2, 3, 4)
+
+
+# 1, 2, 3, 4
 # including metadata into function
-def add(x:int or float, y:int) -> int:
+def add(x: int or float, y: int) -> int:
     """
     Some aditional info
     :param x:
     :param y:
     :return:
     """
-    return x+y
+    return x + y
 
 
 # try to use function with a string instead of int/float
 try:
-    add('str',3)
+    add('str', 3)
 except Exception as e:
     print(e)
 # check how function works
@@ -74,3 +106,16 @@ for f in my_list:
 # 1.5
 # 2.0
 # 2.5
+
+
+# instead of creating simple class we can create a function
+import pandas as pd
+def create_table(data):
+    pd_data = pd.DataFrame(data)
+    print(pd_data)
+    def my_mean(**kwargs):
+        print(pd_data.mean())
+    return my_mean
+
+my_table = create_table([1,2,3,4])
+my_table(some_arg = 'some')
